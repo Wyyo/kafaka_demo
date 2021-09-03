3bd21bd9cb942c8fbcd9f725bda2f5bcba34c4c9
wuyi <2337886287@qq.com>
2021-09-03 17:01:48 +0800

    demo

---
+++ src/main/java/com/example/kafka_demo/com/example/kafka_demo_controller.java
@@ -0,0 +1,18 @@
+package com.example.kafka_demo.com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wuyi
+ * @date 2021/9/3
+ */
+@RestController
+public class kafka_demo_controller {
+    @RequestMapping("/test")
+    public void test(){
+        System.out.println("innnnn");
+        System.out.println("热部署？");
+        System.out.println("boyibo");
+    }
+}
