7a2025183f8b28958012c974c88a2d5dc1257fc8
wuyi <2337886287@qq.com>
2021-09-03 17:56:38 +0800

    dev2第二次提交

--- src/main/java/com/example/kafka_demo/com/example/Dev2.java
+++ src/main/java/com/example/kafka_demo/com/example/Dev2.java
@@ -7,5 +7,6 @@ package com.example.kafka_demo.com.example;
 public class Dev2 {
     public static void main(String[] args) {
         System.out.println("dev2第一次提交");
+        System.out.println("dev2第二次提交");
     }
 }
