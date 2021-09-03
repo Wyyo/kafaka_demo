141c9ca9fbf0b509ed5dcfad02d161dd7b20be6f
wuyi <2337886287@qq.com>
2021-09-03 17:30:14 +0800

    dev2第一次提交

---
+++ src/main/java/com/example/kafka_demo/com/example/Dev2.java
@@ -0,0 +1,11 @@
+package com.example.kafka_demo.com.example;
+
+/**
+ * @author wuyi
+ * @date 2021/9/3
+ */
+public class Dev2 {
+    public static void main(String[] args) {
+        System.out.println("dev2第一次提交");
+    }
+}
